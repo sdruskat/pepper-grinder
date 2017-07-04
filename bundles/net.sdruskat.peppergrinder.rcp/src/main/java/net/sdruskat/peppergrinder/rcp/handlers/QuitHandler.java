cf7c3e2d20e67ccdb1455f75033757afbe381052
Stephan Druskat <mail@sdruskat.net>
2017-07-04 12:52:07 +0200

    Fix copyright and add JavaDoc

--- bundles/net.sdruskat.peppergrinder.rcp/src/main/java/net/sdruskat/peppergrinder/rcp/handlers/QuitHandler.java
+++ bundles/net.sdruskat.peppergrinder.rcp/src/main/java/net/sdruskat/peppergrinder/rcp/handlers/QuitHandler.java
@@ -1,3 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2016, 2017 Stephan Druskat
+ * Exploitation rights for this version belong exclusively to Universität Hamburg
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Stephan Druskat - initial API and implementation
+ *******************************************************************************/
 package net.sdruskat.peppergrinder.rcp.handlers;
 
 import org.eclipse.e4.core.di.annotations.Execute;
@@ -5,13 +24,29 @@ import org.eclipse.e4.ui.workbench.IWorkbench;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.widgets.Shell;
 
+import net.sdruskat.peppergrinder.rcp.LogManager;
 
+/**
+ * Handles closing of the workbench.
+ *
+ * @author Stephan Druskat <[mail@sdruskat.net](mailto:mail@sdruskat.net)>
+ * 
+ */
 public class QuitHandler {
+	
+	private static final LogManager log = LogManager.INSTANCE;
+	
+	/**
+	 * Closes the workbench if prompted user agrees via dialog.
+	 * 
+	 * @param workbench
+	 * @param shell
+	 */
 	@Execute
-	public void execute(IWorkbench workbench, Shell shell){
-		if (MessageDialog.openConfirm(shell, "Confirmation",
-				"Do you want to exit?")) {
+	public void execute(IWorkbench workbench, Shell shell) {
+		if (MessageDialog.openConfirm(shell, "Confirmation", "Do you want to exit?")) {
 			workbench.close();
+			log.info("Goodbye.");
 		}
 	}
 }
--- bundles/net.sdruskat.peppergrinder.rcp/src/main/java/net/sdruskat/peppergrinder/rcp/parts/ConversionWorkflowPart.java
+++ bundles/net.sdruskat.peppergrinder.rcp/src/main/java/net/sdruskat/peppergrinder/rcp/parts/ConversionWorkflowPart.java
@@ -1,16 +1,32 @@
+/*******************************************************************************
+ * Copyright (c) 2016, 2017 Stephan Druskat
+ * Exploitation rights for this version belong exclusively to Universität Hamburg
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Stephan Druskat - initial API and implementation
+ *******************************************************************************/
 package net.sdruskat.peppergrinder.rcp.parts;
 
 import javax.annotation.PostConstruct;
-import javax.inject.Inject;
-
 import org.eclipse.e4.ui.di.Focus;
-import org.eclipse.e4.ui.di.Persist;
-import org.eclipse.e4.ui.model.application.ui.MDirtyable;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.DirectoryDialog;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
+
 import net.sdruskat.peppergrinder.rcp.conversion.ConversionRunner;
 import net.sdruskat.peppergrinder.rcp.conversion.ConversionRunnerBuilder;
 
@@ -20,13 +36,26 @@ import org.eclipse.swt.events.MouseAdapter;
 import org.eclipse.swt.events.MouseEvent;
 import org.eclipse.swt.layout.GridData;
 
+/**
+ * A GUI part providing access to basic functionality.
+ *
+ * @author Stephan Druskat <[mail@sdruskat.net](mailto:mail@sdruskat.net)>
+ * 
+ */
 public class ConversionWorkflowPart {
 	
 	private String corpusDirectoryPath = null;
 
-	@Inject
-	private MDirtyable dirty;
+	private Button btnBrowse;
 
+	/**
+	 * Constructs a simple GUI that lets the user
+	 * browse for a directory containing corpus
+	 * files and start the conversion of these
+	 * corpus files via Pepper.
+	 * 
+	 * @param parent
+	 */
 	@PostConstruct
 	public void createComposite(Composite parent) {
 		parent.setLayout(new GridLayout(1, false));
@@ -46,7 +75,7 @@ public class ConversionWorkflowPart {
 		lblSelectedPath.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, true, false, 1, 1));
 		lblSelectedPath.setText("--");
 		
-		Button btnBrowse = new Button(composite, SWT.NONE);
+		btnBrowse = new Button(composite, SWT.NONE);
 		btnBrowse.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, true, false, 2, 1));
 		btnBrowse.setText("Browse");
 		
@@ -82,14 +111,13 @@ public class ConversionWorkflowPart {
 		});
 	}
 
+	/**
+	 * Sets the focus on the *Browse* button.
+	 * 
+	 */
 	@Focus
 	public void setFocus() {
-//		tableViewer.getTable().setFocus();
+		btnBrowse.setFocus();
 	}
 
-	@Persist
-	public void save() {
-		dirty.setDirty(false);
-	}
-	
 }
