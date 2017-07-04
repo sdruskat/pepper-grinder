ff6d43b04224bdd3ac4b39ce4c18f4309a495a08
Stephan Druskat <mail@sdruskat.net>
2017-07-04 13:45:29 +0200

    Add JavaDoc

--- bundles/net.sdruskat.peppergrinder.rcp/src/main/java/net/sdruskat/peppergrinder/rcp/pepper/GrinderPepperConfiguration.java
+++ bundles/net.sdruskat.peppergrinder.rcp/src/main/java/net/sdruskat/peppergrinder/rcp/pepper/GrinderPepperConfiguration.java
@@ -1,3 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2016, 2017 Stephan Druskat
+ * Exploitation rights for this version belong exclusively to Universität Hamburg
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Stephan Druskat - initial API and implementation
+ *******************************************************************************/
 package net.sdruskat.peppergrinder.rcp.pepper;
 
 import java.io.File;
@@ -17,46 +36,52 @@ import org.osgi.framework.Constants;
 
 import com.google.common.base.Splitter;
 
+import net.sdruskat.peppergrinder.rcp.LogManager;
+
+/**
+ * Represents a configuration setup for Pepper suitable for Pepper Grinder.
+ *
+ * @author Stephan Druskat <[mail@sdruskat.net](mailto:mail@sdruskat.net)>
+ * 
+ */
 public class GrinderPepperConfiguration extends PepperConfiguration {
 
-	
-	/**
-	 * Defines a static logger variable so that it references the {@link org.apache.logging.log4j.Logger} instance named "AtomicPepperConfiguration".
-	 */
-//	private static final Logger log = LogManager.getLogger(AtomicPepperConfiguration.class);
-	
+	private static final LogManager log = LogManager.INSTANCE;
+
+	private static final long serialVersionUID = 1L;
 	/** Folder containing configuration files for Pepper */
 	public static final String FOLDER_PEPPER_CONF = "configuration";
-	
 
 	/**
-	 * Loads the "pepper.properties" file via the 
-	 * {@link Properties#load(java.io.InputStream)} mechanism ultimately
-	 * at the end of the inheritance chain.
+	 * Loads the "pepper.properties" file via the
+	 * {@link Properties#load(java.io.InputStream)} mechanism ultimately at the
+	 * end of the inheritance chain.
 	 * 
 	 */
 	public void load() {
-		File atomicHome = findAtomicHome();
-		File propFile = new File(atomicHome.getAbsolutePath() + "/" + GrinderPepperConfiguration.FOLDER_PEPPER_CONF + "/" + PepperStarterConfiguration.FILE_PEPPER_PROP + "/");
+		File grinderHome = findGrinderHome();
+		File propFile = new File(grinderHome.getAbsolutePath() + "/" + GrinderPepperConfiguration.FOLDER_PEPPER_CONF
+				+ "/" + PepperStarterConfiguration.FILE_PEPPER_PROP + "/");
 		try {
 			load(propFile);
 		}
 		catch (Exception e) {
-//			log.error("Could not load Pepper configuration for Atomic!", e);
+			log.error("Could not load Pepper configuration for Pepper Grinder!");
 		}
 	}
 
 	/**
-	 * Gets the "Atomic home" folder, i.e., the folder where the Atomic executable is located.
+	 * Gets the "Pepper Grinder home" folder, i.e., the folder where the Pepper
+	 * Grinder executable is located.
 	 *
-	 * @return pepperHome The pepperHome File
+	 * @return grinderHome The grinderHome File
 	 */
-	private File findAtomicHome() {
-		File atomicHome = null;
-		URL atomicHomeURL = null;
-		atomicHomeURL = Platform.getInstallLocation().getURL();
-		atomicHome = new File(atomicHomeURL.getFile());
-		return atomicHome;
+	private File findGrinderHome() {
+		File grinderHome = null;
+		URL grinderHomeURL = null;
+		grinderHomeURL = Platform.getInstallLocation().getURL();
+		grinderHome = new File(grinderHomeURL.getFile());
+		return grinderHome;
 	}
 
 	/**
@@ -65,30 +90,29 @@ public class GrinderPepperConfiguration extends PepperConfiguration {
 	 * @return plugIn path
 	 */
 	public String getPlugInPath() {
-		return (findAtomicHome().getAbsolutePath() + "/" + this.getProperty(PepperStarterConfiguration.PROP_PLUGIN_PATH));
+		return (findGrinderHome().getAbsolutePath() + "/"
+				+ this.getProperty(PepperStarterConfiguration.PROP_PLUGIN_PATH));
 	}
 
 	/**
-	 * Returns the dropin paths for OSGi bundles.
-	 * FIXME: These are not used in the contenxt of
-	 * Atomic.
+	 * Returns the dropin paths for OSGi bundles. FIXME: These are not used in
+	 * the context of Pepper Grinder.
 	 *
 	 * @return List<String> the dropin paths
 	 */
 	public List<String> getDropInPaths() {
-			String rawList = this.getProperty(PepperStarterConfiguration.PROP_DROPIN_PATHS);
-			if (rawList != null) {
-				Iterator<String> it
-					= Splitter.on(',').trimResults().omitEmptyStrings().split(rawList).iterator();
-				List<String> result = new ArrayList<>();
-				while (it.hasNext()) {
-					result.add(it.next());
-				}
-				return result;
+		String rawList = this.getProperty(PepperStarterConfiguration.PROP_DROPIN_PATHS);
+		if (rawList != null) {
+			Iterator<String> it = Splitter.on(',').trimResults().omitEmptyStrings().split(rawList).iterator();
+			List<String> result = new ArrayList<>();
+			while (it.hasNext()) {
+				result.add(it.next());
 			}
-			return null;
+			return result;
+		}
+		return null;
 	}
-	
+
 	/**
 	 * Returns a temporary path, where the entire system and all modules can
 	 * store temp files. If no temp folder is given by configuration file, the
@@ -104,10 +128,11 @@ public class GrinderPepperConfiguration extends PepperConfiguration {
 			tmpFolder = new File(tmpFolderStr);
 			if (!tmpFolder.exists()) {
 				if (!tmpFolder.mkdirs()) {
-//					log.warn("Cannot create folder {}. ", tmpFolder);
+					log.warn("Cannot create folder " + tmpFolder + ".");
 				}
 			}
-		} else {
+		}
+		else {
 			tmpFolder = PepperUtil.getTempFile();
 		}
 		return (tmpFolder);
