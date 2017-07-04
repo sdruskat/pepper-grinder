f8d6dfe04233222ddbb3f09ff59686f7703fe6ff
Stephan Druskat <mail@sdruskat.net>
2017-07-04 10:28:03 +0200

    Add copyright and log

--- bundles/net.sdruskat.peppergrinder.rcp/src/main/java/net/sdruskat/peppergrinder/rcp/Activator.java
+++ bundles/net.sdruskat.peppergrinder.rcp/src/main/java/net/sdruskat/peppergrinder/rcp/Activator.java
@@ -1,9 +1,38 @@
+/*******************************************************************************
+ * Copyright (c) 2016, 2017 Stephan Druskat
+ * Exploitation rights for this version belong exclusively to Universität Hamburg
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Stephan Druskat - initial API and implementation
+ *******************************************************************************/
 package net.sdruskat.peppergrinder.rcp;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+/**
+ * Activator for bundle `net.sdruskat.peppergrinder.rcp`.
+ *
+ * @author Stephan Druskat <[mail@sdruskat.net](mailto:mail@sdruskat.net)>
+ * 
+ */
 public class Activator implements BundleActivator {
+	
+	Logger log = LoggerFactory.getLogger(Activator.class);
 
 	private static BundleContext context;
 
@@ -17,6 +46,7 @@ public class Activator implements BundleActivator {
 	 */
 	public void start(BundleContext bundleContext) throws Exception {
 		Activator.context = bundleContext;
+		log.trace("Start bundle '{}'.", context.getBundle().getSymbolicName());
 	}
 
 	/*
@@ -24,6 +54,7 @@ public class Activator implements BundleActivator {
 	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
 	 */
 	public void stop(BundleContext bundleContext) throws Exception {
+		log.trace("Stop bundle '{}'.", context.getBundle().getSymbolicName());
 		Activator.context = null;
 	}
 
