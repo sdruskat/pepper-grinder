a9465e3587d1e1541f23b0d2695ea827157517c9
Stephan Druskat <mail@sdruskat.net>
2017-11-03 16:51:05 +0100

    Update pre-release script to *really* change version in citation metadata in README.md

--- releng/prepare-release.sh
+++ releng/prepare-release.sh
@@ -17,7 +17,8 @@ echo 'Updating version in doc POM'
 mvn -f ./releng/net.sdruskat.peppergrinder.doc/pom.xml versions:set -DnewVersion=$1
 echo 'Replace config version in root POM with sed'
 sed -i -e '/<artifactId>net.sdruskat.peppergrinder.configuration<\/artifactId>/!b;n;c        <version>'$1'</version>' pom.xml 
-sed -i -e '/> Stephan Druskat. Pepper Grinder \(v.*/> Stephan Druskat. Pepper Grinder (v'$1'). Zenodo. https://doi.org/10.5281/zenodo.1041735.' README.md
+echo 'Replace citation metadata in README.md'
+sed -i '/>\sStephan\sDruskat\.\sPepper\sGrinder\s(v.*/c\> Stephan Druskat. Pepper Grinder (v'$1'). Zenodo. https://doi.org/10.5281/zenodo.1041735.' README.md
 
 # Build documentation
-mvn -f ./releng/net.sdruskat.peppergrinder.doc/pom.xml clean package -P docs
+#mvn -f ./releng/net.sdruskat.peppergrinder.doc/pom.xml clean package -P docs
